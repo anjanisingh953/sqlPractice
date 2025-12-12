0728c31b1907420a55d27f1f176f87818ab87842
Anjani Singh <anjanisingh953@gmail.com>
2025-12-12 15:13:02 +0530

    created best questions for practice_04

--- SQL_practice_04/04_questions.sql
+++ SQL_practice_04/04_questions.sql
@@ -9,4 +9,4 @@
 -- 4.7 Show the titles of movies not currently being shown in any theaters.
 -- 4.8 Add the unrated movie "One, Two, Three".
 -- 4.9 Set the rating of all unrated movies to "G".
--- 4.10 Remove movie theaters projecting movies rated "NC-17".
+-- 4.10 Remove movie theaters projecting movies rated "NC-17". 
